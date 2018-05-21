1698efb539e40b38b6872927b53ebac60e264d62
Filipe M. Silva <filipe@smartrips.co>
2018-05-21 20:05:54 -0300

    test operations and adjust handle with insert

---
+++ test/lib/operations.test.js
@@ -0,0 +1,150 @@
+'use strict';
+
+const assert = require('assert');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const Operations = require('../../lib/operations');
+const Connections = require('../../lib/connections');
+
+describe('Unit test for operations', () => {
+
+  let collectionName,
+      mockConnection,
+      operations,
+      connObject;
+
+  beforeEach(() => {
+    collectionName = 'collectionTest';
+    mockConnection = new Promise((resolve, reject) => {
+
+    });
+    operations = new Operations(collectionName, mockConnection);
+  });
+
+  // afterEach(() => operations.connection.restore());
+
+  context('Operations()', () => {
+    it('expect operations has connection and collectionName as property', () => {
+      expect(operations).to.have.property('connection');
+      expect(operations).to.have.property('collectionName');
+
+      expect(operations.connection).to.be.equal(mockConnection);
+      expect(operations.collectionName).to.be.equal(collectionName);
+    });
+  });
+
+  context('createPromise', () => {
+    it('expect return a object after call createPromise', () => {
+      const result = operations.createPromise((res, rej) => {
+        res();
+      });
+
+      expect(typeof result).to.be.equal('object');
+    });
+  });
+
+  context('retriveCollection()', () => {
+    it('expect spy be called after call retriveCollection', () => {
+      let db = {
+        collection: sinon.spy()
+      };
+
+      operations.retriveCollection(db);
+
+      expect(db.collection.called).to.be.true;
+      expect(db.collection.calledWith(sinon.match(String))).to.be.true;
+      expect(db.collection.calledWith('collectionTest')).to.be.true;
+    });
+  });
+
+  context('insert()', () => {
+    let sandbox,
+        dbMockObject,
+        spyCreatePromise,
+        spyCheckForData,
+        spyRetriveCollection,
+        collectionMock;
+
+    beforeEach(() => {
+      sandbox = sinon.createSandbox();
+
+      collectionMock = {
+        insertMany: sinon.stub(),
+        insert: sinon.stub()
+      };
+
+      dbMockObject = {
+        close: sandbox.spy(),
+        collection: sandbox.stub().returns(collectionMock)
+      };
+
+      spyCreatePromise = sandbox.spy(operations, 'createPromise');
+      spyCheckForData = sandbox.spy(operations, 'checkForData');
+      spyRetriveCollection = sandbox.spy(operations, 'retriveCollection');
+
+      operations.connection = new Promise((resolve) => {
+        resolve(dbMockObject);
+      });
+
+      operations.insert({ test: 'teste' });
+    });
+
+    afterEach(() => sandbox.restore());
+
+    it('expect call createPromise', () => {
+      operations.insert({ test: 'teste' });
+      expect(spyCreatePromise.called).to.be.true;
+    });
+
+    it('expect call checkForData', () => {
+      operations.insert({ test: 'teste' });
+      expect(spyCheckForData.called).to.be.true;
+    });
+
+    it('expect call retriveCollection', (done) => {
+      collectionMock.insert.callsFake((arg1, arg2, cb) => {
+        cb(null, true);
+      });
+
+      operations.insert({ test: 'teste' }).then((result) => {
+        expect(spyRetriveCollection.called).to.be.true;
+        done();
+      });
+    });
+
+    it('expect close called after complete operations', (done) => {
+      collectionMock.insert.callsFake((arg1, arg2, cb) => {
+        cb(null, true);
+      });
+
+      operations.insert({ test: 'teste' }).then((result) => {
+        expect(dbMockObject.close.called).to.be.true;
+        done();
+      });
+    });
+
+    it('expect call insert', (done) => {
+      collectionMock.insert.callsFake((arg1, arg2, cb) => {
+        cb(null, true);
+      });
+
+      operations.insert({ test: 'teste' }).then((result) => {
+        expect(collectionMock.insert.called).to.be.true;
+        done();
+      });
+    });
+
+    it('expect call insertMany for array data', (done) => {
+      collectionMock.insertMany.callsFake((arg1, arg2, cb) => {
+        cb(null, true);
+      });
+
+      operations.insert([{ test: 'teste' }]).then((result) => {
+        expect(collectionMock.insertMany.called).to.be.true;
+        done();
+      });
+    });
+
+  });
+});
